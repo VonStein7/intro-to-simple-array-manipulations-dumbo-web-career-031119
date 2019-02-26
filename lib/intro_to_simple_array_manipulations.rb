7ee85805e5b834393d6fb68b854f8d0698a3adc9
brenden williams <brendenw7@gmail.com>
2019-02-26 01:20:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,3 +13,36 @@ end
 def pop_with_args(arr)
   arr.pop(2)
 end 
+
+def using_shift(arr)
+  arr.shift 
+end
+
+def shift_with_args
+  arr.shift(2)
+end
+
+def using_concat(arr1,arr2)
+  arr1.concat(arr2)
+end
+
+def using_insert
+  
+end
+
+def using_uniq
+  
+end
+
+def using_flatten
+  
+end
+
+def using_delete
+  
+end
+
+def using_delete_at
+
+end
+
